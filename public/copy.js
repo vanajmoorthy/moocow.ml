1eab4dd8ad6e7d1d0f0865cf15b74c03c3c10f66
vanajmoorthy <vanajmoorthy@gmail.com>
2020-07-16 22:49:51 +0530

    change copy mechanism so that it actually works

--- public/copy.js
+++ public/copy.js
@@ -1,25 +1,20 @@
-const button = document.getElementById("copy");
-const link = document.getElementById("link").href;
+
+
+let clipboard = new ClipboardJS('.far');
+
+clipboard.on('success', function (e) {
+    console.info('Text:', e.text);
+    e.clearSelection();
+});
 
 let div = document.getElementById("output")
-
-button.addEventListener("click", copy);
+div.addEventListener("click", copy)
 
 function copy() {
-
-    let temp = document.createElement("textarea");
-    document.body.appendChild(temp);
-    temp.value = link
-    temp.select();
-
-    document.execCommand("copy");
-
     if (!document.getElementById("p")) {
         let p = document.createElement("p")
         div.appendChild(p)
         p.id = "p"
         p.innerText = "URL copied successfully!"
     }
-
-    document.body.removeChild(temp)
 }
