14ef1549327ebc04aa693573bc38d0200c10202a
Restyled.io <commits@restyled.io>
2021-05-20 03:17:38 +0000

    Restyled by clang-format

--- models/User.js
+++ models/User.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
 
 const User = new mongoose.Schema({
-	long: {
-		type: String,
-		required: true,
-	},
-	short: {
-		type: String,
-		required: true,
-	},
-	type: {
-		type: String,
-		required: true,
-	},
-	clicks: {
-		type: Number,
-		required: true,
-		default: 0,
-	},
+  long : {
+    type : String,
+    required : true,
+  },
+  short : {
+    type : String,
+    required : true,
+  },
+  type : {
+    type : String,
+    required : true,
+  },
+  clicks : {
+    type : Number,
+    required : true,
+    default : 0,
+  },
 });
 
 module.exports = mongoose.model("short", shortSchema);
